2f11d2213cbf728a0464ecc1c54557d2840afcdc
KristinaCer <k.cerniauskaite@gmail.com>
2021-02-03 10:27:52 +0100

    added updated launch files

---
+++ EmployeeAPI/Migrations/20210202210310_EmployeeRoleRelation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeAPI.Migrations
+{
+    public partial class EmployeeRoleRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
